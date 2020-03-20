59a392c48567ed9bc4b404483c111e950b8224f5
Gberger-98 <gabriel.berger98@gmail.com>
2020-03-20 11:42:55 +0100

    Fix de validation_creation.php

--- validation_creation.php
+++ validation_creation.php
@@ -11,38 +11,42 @@ switch(isset($_POST['mail'], $_POST['password'], $_POST['nom'], $_POST['prenom']
     
     //si le formulaire n'est pas entièrement rempli
     case empty($_POST['mail']):
-        echo "Vous devez renseigner votre adresse mail";
-    break;
-    case empty($_POST['mail']):
-        echo "Vous devez renseigner votre mot de passe";
-    break;
-    case empty($_POST['mail']):
-        echo "Vous devez renseigner votre nom";
-    break;
-    case empty($_POST['mail']):
-        echo "Vous devez renseigner votre prénom";
-    break;
-    case empty($_POST['mail']):
-        echo "Vous devez renseigner votre date de naissance";
-    break;
-    case empty($_POST['mail']):
-        echo "Vous devez renseigner votre identifiant";
+        echo "Vous devez renseigner votre adresse mail."; 
+        echo "</br>";
+    case empty($_POST['password']):
+        echo "Vous devez renseigner votre mot de passe.";
+        echo "</br>";
+    case empty($_POST['nom']):
+        echo "Vous devez renseigner votre nom.";
+        echo "</br>";
+    case empty($_POST['prenom']):
+        echo "Vous devez renseigner votre prénom.";
+        echo "</br>";
+    case empty($_POST['date']):
+        echo "Vous devez renseigner votre date de naissance.";
+        echo "</br>";
+    case empty($_POST['username']):
+        echo "Vous devez renseigner votre identifiant.";
+        echo "</br>";
     break;
 
     //conditions sur l'identifiant
     case (!preg_match("#^[a-z0-9]+$#",$_POST['username'])):
         echo "L'identifiant doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+        echo "</br>";
     break;
     case strlen($_POST['username'])>25:
         echo "L'identifiant est trop long, il dépasse 25 caractères.";
     break;
     case (mysqli_num_rows(mysqli_query($bdd_users,"SELECT * FROM Utilisateur WHERE Pseudo='".$_POST['username']."'"))==1):
         echo "Cet identifiant est déjà utilisé.";
+        echo "</br>";
     break;
 
     //conditions sur l'adresse mail
     case (mysqli_num_rows(mysqli_query($bdd_users,"SELECT * FROM Utilisateur WHERE mail='".$_POST['mail']."'"))==1):
         echo "Cette adresse mail est déjà utilisée.";
+        echo "</br>";
     break;
 
     default:

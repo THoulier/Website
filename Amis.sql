fd92da5cac0aecfc32e5967e037ecebfa4fd3ae8
THoulier <56867720+THoulier@users.noreply.github.com>
2020-03-23 16:13:27 +0100

    Ajout table amis

---
+++ Amis.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : lun. 23 mars 2020 à 16:12
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Amis`
+--
+
+CREATE TABLE `Amis` (
+  `ID` int(11) NOT NULL,
+  `id_from` int(11) NOT NULL,
+  `id_to` int(11) NOT NULL,
+  `etat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Amis`
+--
+
+INSERT INTO `Amis` (`ID`, `id_from`, `id_to`, `etat`) VALUES
+(1, 3, 2, 0),
+(10, 2, 1, 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Amis`
+--
+ALTER TABLE `Amis`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_from` (`id_from`),
+  ADD KEY `id_to` (`id_to`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Amis`
+--
+ALTER TABLE `Amis`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Amis`
+--
+ALTER TABLE `Amis`
+  ADD CONSTRAINT `Amis_ibfk_1` FOREIGN KEY (`id_from`) REFERENCES `Utilisateur` (`ID`),
+  ADD CONSTRAINT `Amis_ibfk_2` FOREIGN KEY (`id_to`) REFERENCES `Utilisateur` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

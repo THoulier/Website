c8ecc5673fc982bbdd806780aeadfdc860496766
Gberger-98 <gabriel.berger98@gmail.com>
2020-03-30 13:23:52 +0200

    Mise en page de creation.php

--- page_creation.php
+++ page_creation.php
@@ -51,93 +51,150 @@
      </div>
   </div>
 
-    <div class="container ">
-
+  <div class="container">
+    <form action="validation_creation.php" method="post">
+      <div class="row">
         <div class="col-sm ">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <form action="validation_creation.php" method="post">
-
-                Identifiant: <input type="text" name="username" class="p-2"/>
-                <?php
-                if (isset($_GET["id"])){ 
-                  if ($_GET["id"]==1) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "Vous devez renseigner votre identifiant !";
-                    echo "</div>";
-                  } elseif ($_GET["id"]==2) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "L'identifiant doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
-                    echo "</div>";
-                  } elseif ($_GET["id"]==3) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "L'identifiant est trop long, il dépasse 25 caractères.";
-                    echo "</div>";
-                  } elseif ($_GET["id"]==4) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "Cet identifiant est déjà utilisé.";
-                    echo "</div>";
-                  }
-                }
-                ?>
-                Nom: <input type="text" name="nom" class="p-2"/> 
-                <?php
-                if (isset($_GET["nom"])) {
-                  echo '<div class="alert alert-danger" role="alert">';
-                        echo "Vous devez renseigner votre nom.";
-                  echo "</div>";
-                }
-                ?>
-                Prénom: <input type="text" name="prenom" class="p-2"/> 
-                <?php
-                if (isset($_GET["prenom"])) {
-                  echo '<div class="alert alert-danger" role="alert">';
-                        echo "Vous devez renseigner votre prénom.";
-                  echo "</div>";
-                }
-                ?>
-                Date de naissance: <input type="date" name="date" class="p-2"/> </br>
-                <?php
-                if (isset($_GET["date"])) {
-                  echo '<div class="alert alert-danger" role="alert">';
-                        echo "Vous devez renseigner votre date de naissance.";
-                  echo "</div>";
-                }
-                ?>
-                Mot de passe: <input type="password" name="password" class="p-2"/> 
-                <?php
-                if (isset($_GET["pass"])) {
-                  echo '<div class="alert alert-danger" role="alert">';
-                        echo "Vous devez renseigner votre mot de passe.";
-                  echo "</div>";
-                }
-                ?>
-                E-mail: <input type="email" name="mail" class="p-2"/> 
-                <?php
-                if (isset($_GET["mail"])) {
-                  if ($_GET["mail"]==1) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "Vous devez renseigner votre adresse mail.";
-                    echo "</div>";
-                  } elseif ($_GET["mail"]==2) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                          echo "Cette adresse mail est déjà utilisée.";
-                    echo "</div>";
-                  }
-                }
-                ?>
-
-              <button type="submit" class="btn btn-primary">Inscrivez-vous !</button>
-            </form>
-            </div>
-          </div>
+          Identifiant: 
         </div>
-    </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="text" name="username" class="p-2"/>
+        </div>
+        <div class="col-sm ">
+          <?php
+          if (isset($_GET["id"])){ 
+            if ($_GET["id"]==1) {
+              echo '<div class="alert alert-danger" role="alert">';
+                  echo "Vous devez renseigner votre identifiant !";
+              echo "</div>";
+            } elseif ($_GET["id"]==2) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "L'identifiant doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+              echo "</div>";
+            } elseif ($_GET["id"]==3) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "L'identifiant est trop long, il dépasse 25 caractères.";
+              echo "</div>";
+            } elseif ($_GET["id"]==4) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "Cet identifiant est déjà utilisé.";
+              echo "</div>";
+            }
+          }
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm ">
+          Nom:  
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="text" name="nom" class="p-2"/>
+        </div>
+        <div class="col-sm ">
+        <?php
+          if (isset($_GET["nom"])) {
+            echo '<div class="alert alert-danger" role="alert">';
+              echo "Vous devez renseigner votre nom.";
+            echo "</div>";
+          }
+        ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm ">
+          Prénom:  
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="text" name="prenom" class="p-2"/>
+        </div>
+        <div class="col-sm">
+          <?php
+            if (isset($_GET["prenom"])) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "Vous devez renseigner votre prénom.";
+              echo "</div>";
+            }
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm ">
+          Date de naissance: 
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="date" name="date" class="p-2"/> </br>
+        </div>
+        <div class="col-sm">
+          <?php
+            if (isset($_GET["date"])) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "Vous devez renseigner votre date de naissance.";
+              echo "</div>";
+            }
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm ">
+          Mot de passe: 
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="password" name="password" class="p-2"/>
+        </div>
+        <div class="col-sm">
+          <?php
+            if (isset($_GET["pass"])) {
+              echo '<div class="alert alert-danger" role="alert">';
+                echo "Vous devez renseigner votre mot de passe.";
+              echo "</div>";
+            }
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm ">
+          E-mail: 
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <input type="email" name="mail" class="p-2"/>
+        </div>
+        <div class="col-sm">
+          <?php
+            if (isset($_GET["mail"])) {
+              if ($_GET["mail"]==1) {
+                echo '<div class="alert alert-danger" role="alert">';
+                  echo "Vous devez renseigner votre adresse mail.";
+                echo "</div>";
+              } elseif ($_GET["mail"]==2) {
+                echo '<div class="alert alert-danger" role="alert">';
+                  echo "Cette adresse mail est déjà utilisée.";
+                echo "</div>";
+              }
+            }
+          ?>
+        </div>
+      </div>
+      <div class ="row">
+        <button type="submit" class="btn btn-primary">Inscrivez-vous !</button>
+      </div>
+    </form>
+  </div>
 
-    
-
-	</body>
-  <footer class="text-muted">
+</body>
+<footer class="text-muted">
   <div class="container pt-5">
     <p class="float-right">
       <a href="#">Back to top</a>

db287a6651bc7384bdbfc1bd969acf4dfe1c9a85
gberger-98 <gabriel.berger98@gmail.com>
2020-04-06 16:50:59 +0200

    fix des tableaux et des div

--- ajouter_ami.php
+++ ajouter_ami.php
@@ -3,9 +3,6 @@
     session_start();
     include("ressource/head.php");
 ?>
-<!DOCTYPE html>
-
-<html lang="en">
 
 <body>
 
@@ -16,141 +13,132 @@
 
 
   <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-     <div class="col-md-6 pt-4 px-0">
-       <h1 class="display-4 font-italic"><p id="welcome">Gestion des amis</p></h1>
-       <p class="lead my-3">Rentre l'identifiant de ton ami dans le formulaire et ajoute le!</p>
+    <div class="col-md-6 pt-4 px-0">
+      <h1 class="display-4 font-italic"><p id="welcome">Gestion des amis</p></h1>
+      <p class="lead my-3">Rentre l'identifiant de ton ami dans le formulaire et ajoute le!</p>
        
-     </div>
-   </div>
+    </div>
+  </div>
 
 
   <div class="container marketing">
-
-
-<div class="row ">
-  <div class="bg-white shadow p-5 mb-5 text-dark text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
-  <div class="titre"><h2>Envoies une invitation</h2></div>
-
-  <form action="validation_ajout_ami.php" method="post">
-
-    Identifiant ou adresse mail: <input type="text" name="id_to" class="p-2"/> </br>
-    <?php
-      if (isset($_GET["id"])){ 
-        if ($_GET["id"]==1) {
-          echo '<div class="alert alert-danger" role="alert">';
-          echo "Vous devez renseigner un identifiant ou une adresse mail";
-          echo "</div>";
-        } 
-        elseif ($_GET["id"]==2) {
-          echo '<div class="alert alert-danger" role="alert">';
-          echo "Vous avez déjà envoyé une demande à cet utilisateur";
-          echo "</div>";
-        }
-        elseif ($_GET["id"]==3) {
-          echo '<div class="alert alert-success" role="alert">Votre demande a été envoyée!</div>';
-        }
-        elseif ($_GET["id"]==4) {
-          echo '<div class="alert alert-danger" role="alert">';
-          echo "Cet utilisateur n'existe pas";
-          echo "</div>";
-        }
-        elseif ($_GET["id"]==5) {
-          echo '<div class="alert alert-danger" role="alert">Vous avez déjà reçue une demande de cet utilisateur ou il est déjà votre ami. </div>';
-        }
-        elseif ($_GET["id"]==6) {
-          echo '<div class="alert alert-danger" role="alert">Vous ne pouvez vous demander en ami! </div>';
-        }
-      }
-    ?>
-<input type="submit" value="Ajouter en ami"/>
-</form>
-  </div>
-  <div class="bg-light shadow p-5 mb-5 text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
-
-  <div class="titre"><h2>Invitations reçues</h2></div>
-    <?php
-      $bdd_friend = con();
-      if (mysqli_connect_errno($bdd_friend)) {
-          echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
-      }
-
-      $cpt = 1;
-      $ras = mysqli_query($bdd_friend, "SELECT Amis.etat,Amis.id_from,Amis.ID FROM Utilisateur INNER JOIN Amis ON Amis.id_to=Utilisateur.ID WHERE Utilisateur.Mail='".$_SESSION['mail']."'");
-      echo '<table class="table table-hover table-white">';
-      echo '<tbody>';
-
-      while ($donnees = ($row = mysqli_fetch_row($ras))){
-        $res = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo FROM Utilisateur INNER JOIN Amis ON Amis.id_from=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[1]."'");
-        $raw=mysqli_fetch_row ($res); 
-        if($donnees[0]==0){
-          echo '<tr>';
-          echo '<th scope="row">'.$cpt.'</th>';
-          echo '<td>'.$raw[0].'</td>';
-          echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=add&id=". $donnees[2] . "'> Ajouter</a></br></td>";
-          echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[2] . "'> Supprimer</a></td>";
-          echo '</tr>';
-          $cpt +=1;
-        }}
-    
-      echo '</tbody>';
-      echo '</table>'  
-    ?>
-           
-
-    </div>
-</div>
-
-
-
-
-  <div class="bg-white shadow p-5 mb-5 text-dark text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
-
-  <div class="titre"><h2>Tes amis</h2></div>
-    <?php
-      
-      $bdd_friend = con();
-      if (mysqli_connect_errno($bdd_friend)) {
-          echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
-      }
-     
-      $cpt = 1;
-      $ras = mysqli_query($bdd_friend, "SELECT etat,id_from,id_to,ID,Solde FROM  Amis ");
-      echo '<table class="table table-hover table-white">';
-      echo '<tbody>';
-          while ($donnees = ($row = mysqli_fetch_row($ras))){
-              if($donnees[0]==1){
-                if($donnees[2]==$_SESSION['ID']){
-                  $res1 = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo,Utilisateur.Nom,Utilisateur.Prenom FROM Utilisateur INNER JOIN Amis ON Amis.id_from=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[1]."'");
-                  $raw1 = mysqli_fetch_row($res1);
-                  echo '<tr>';
-                  echo '<th scope="row">'.$cpt.'</th>';
-                  echo '<td>'.$raw1[1].'</td>';
-                  echo '<td>'.$raw1[2].'</td>';
-                  echo '<td>('.$raw1[0].')</td>';
-                  echo '<td>'.$donnees[4].'€</td>';
-                  echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[3] . "'> Supprimer</a></td>";
-                  echo '</tr>';
-                  $cpt +=1;
+    <div class="row ">
+      <div class="bg-white shadow p-5 mb-5 text-dark text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
+        <div class="titre"><h2>Envoies une invitation</h2></div>
+          <form action="validation_ajout_ami.php" method="post">
+            Identifiant ou adresse mail: <input type="text" name="id_to" class="p-2"/> </br>
+            <?php
+              if (isset($_GET["id"])){ 
+                if ($_GET["id"]==1) {
+                  echo '<div class="alert alert-danger" role="alert">';
+                  echo "Vous devez renseigner un identifiant ou une adresse mail";
+                  echo "</div>";
+                } 
+                elseif ($_GET["id"]==2) {
+                  echo '<div class="alert alert-danger" role="alert">';
+                  echo "Vous avez déjà envoyé une demande à cet utilisateur";
+                  echo "</div>";
                 }
-                if ($donnees[1]==$_SESSION['ID']){
-                  $res = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo,Utilisateur.Nom,Utilisateur.Prenom FROM Utilisateur INNER JOIN Amis ON Amis.id_to=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[2]."'");
-                  $raw = mysqli_fetch_row($res);
-                  echo '<tr>';
-                  echo '<th scope="row">'.$cpt.'</th>';
-                  echo '<td>'.$raw[1].'</td>';
-                  echo '<td>'.$raw[2].'</td>';
-                  echo '<td>('.$raw[0].')</td>';
-                  echo '<td>'.-$donnees[4].'€</td>';
-                  echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[3] . "'> Supprimer</a></td>";
-                  echo '</tr>';
-                  $cpt +=1;
+                elseif ($_GET["id"]==3) {
+                  echo '<div class="alert alert-success" role="alert">Votre demande a été envoyée!</div>';
+                }
+                elseif ($_GET["id"]==4) {
+                  echo '<div class="alert alert-danger" role="alert">';
+                  echo "Cet utilisateur n'existe pas";
+                  echo "</div>";
+                }
+                elseif ($_GET["id"]==5) {
+                  echo '<div class="alert alert-danger" role="alert">Vous avez déjà reçue une demande de cet utilisateur ou il est déjà votre ami. </div>';
+                }
+                elseif ($_GET["id"]==6) {
+                  echo '<div class="alert alert-danger" role="alert">Vous ne pouvez vous demander en ami! </div>';
                 }
               }
-          }
-          
-    ?>
-   
-</div>
+            ?>
+            <input type="submit" value="Ajouter en ami"/>
+          </form>
+        </div>
+        <div class="bg-light shadow p-5 mb-5 text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
+          <div class="titre"><h2>Invitations reçues</h2></div>
+            <?php
+              $bdd_friend = con();
+              if (mysqli_connect_errno($bdd_friend)) {
+                  echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
+              }
+            
+              $cpt = 1;
+              $ras = mysqli_query($bdd_friend, "SELECT Amis.etat,Amis.id_from,Amis.ID FROM Utilisateur INNER JOIN Amis ON Amis.id_to=Utilisateur.ID WHERE Utilisateur.Mail='".$_SESSION['mail']."'");
+              echo '<table class="table table-hover table-white">';
+              echo '<tbody>';
+            
+              while ($donnees = ($row = mysqli_fetch_row($ras))){
+                $res = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo FROM Utilisateur INNER JOIN Amis ON Amis.id_from=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[1]."'");
+                $raw=mysqli_fetch_row ($res); 
+                if($donnees[0]==0){
+                  echo '<tr>';
+                  echo '<th scope="row">'.$cpt.'</th>';
+                  echo '<td>'.$raw[0].'</td>';
+                  echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=add&id=". $donnees[2] . "'> Ajouter</a></br></td>";
+                  echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[2] . "'> Supprimer</a></td>";
+                  echo '</tr>';
+                  $cpt +=1;
+                }}
+              
+              echo '</tbody>';
+              echo '</table>'  
+            ?>
+          </div>
+        </div>
+        <div class="bg-white shadow p-5 mb-5 text-dark text-center mr-md-3 pt-3 px-3 pt-md-5 px-md-5 col">
+          <div class="titre"><h2>Tes amis</h2></div>
+            <?php
+              $bdd_friend = con();
+              if (mysqli_connect_errno($bdd_friend)) {
+                echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
+              }
+              $cpt = 1;
+              $ras = mysqli_query($bdd_friend, "SELECT etat,id_from,id_to,ID,Solde FROM  Amis ");
+              echo '<table class="table table-hover table-white">';
+                echo '<tbody>';
+                  while ($donnees = ($row = mysqli_fetch_row($ras))) {
+                    if($donnees[0]==1){
+                      if($donnees[2]==$_SESSION['ID']){
+                        $res1 = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo,Utilisateur.Nom,Utilisateur.Prenom FROM Utilisateur INNER JOIN Amis ON Amis.id_from=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[1]."'");
+                        $raw1 = mysqli_fetch_row($res1);
+                        echo '<tr>';
+                        echo '<th scope="row">'.$cpt.'</th>';
+                        echo '<td>'.$raw1[1].'</td>';
+                        echo '<td>'.$raw1[2].'</td>';
+                        echo '<td>('.$raw1[0].')</td>';
+                        echo '<td>'.$donnees[4].'€</td>';
+                        echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[3] . "'> Supprimer</a></td>";
+                        echo '</tr>';
+                        $cpt +=1;
+                      }
+                      if ($donnees[1]==$_SESSION['ID']){
+                        $res = mysqli_query($bdd_friend, "SELECT Utilisateur.Pseudo,Utilisateur.Nom,Utilisateur.Prenom FROM Utilisateur INNER JOIN Amis ON Amis.id_to=Utilisateur.ID WHERE Utilisateur.ID='".$donnees[2]."'");
+                        $raw = mysqli_fetch_row($res);
+                        echo '<tr>';
+                        echo '<th scope="row">'.$cpt.'</th>';
+                        echo '<td>'.$raw[1].'</td>';
+                        echo '<td>'.$raw[2].'</td>';
+                        echo '<td>('.$raw[0].')</td>';
+                        echo '<td>'.-$donnees[4].'€</td>';
+                        echo "<td><a class='btn btn-primary' role='button' href='action_ami.php?action=delete&id=". $donnees[3] . "'> Supprimer</a></td>";
+                        echo '</tr>';
+                        $cpt +=1;
+                      }
+                    }
+                  }
+                echo '</tbody>';
+              echo '</table>';  
+
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 
--- transactions.php
+++ transactions.php
@@ -18,15 +18,14 @@
        <p class="lead my-3">Tu peux consulter tes transactions avec tes amis!</p>
        
      </div>
-   </div>
-
-
-   <div class="container" id="main">
-   <div class="row">
-
-  <h2>Ajoute une transaction</h2>
   </div>
-      <form action="validation_ajout_transaction.php" method="post">
+
+
+  <div class="container" id="main">
+    <div class="row">
+      <h2>Ajoute une transaction</h2>
+    </div>
+    <form action="validation_ajout_transaction.php" method="post">
       <div class="row">
         <div class="col-sm ">
           Message explicatif: 
@@ -38,13 +37,13 @@
         </div>
         <div class="col-sm-8">
           <?php
-          if (isset($_GET["msg_ex"])){ 
-            if ($_GET["msg_ex"]==1) {
-              echo '<div class="alert alert-danger" role="alert">';
-                  echo "Vous devez renseigner un message explicatif !";
-              echo "</div>";
+            if (isset($_GET["msg_ex"])){ 
+              if ($_GET["msg_ex"]==1) {
+                echo '<div class="alert alert-danger" role="alert">';
+                    echo "Vous devez renseigner un message explicatif !";
+                echo "</div>";
+              }
             }
-          }
           ?>
         </div>
       </div>
@@ -58,25 +57,25 @@
           <input type="text" name="pseudo" class="p-2"/>
         </div>
         <div class="col-sm-8">
-        <?php
-        if (isset($_GET["pseudo"])) {
-            if ($_GET["pseudo"]==1) {
-                echo '<div class="alert alert-danger" role="alert">';
-                    echo "Vous devez renseigner un pseudo ou une adresse mail d'un ami!";
-                echo "</div>";
+          <?php
+            if (isset($_GET["pseudo"])) {
+                if ($_GET["pseudo"]==1) {
+                    echo '<div class="alert alert-danger" role="alert">';
+                        echo "Vous devez renseigner un pseudo ou une adresse mail d'un ami!";
+                    echo "</div>";
+                }
+                if ($_GET["pseudo"]==2) {
+                    echo '<div class="alert alert-danger" role="alert">';
+                        echo "Cet utilisateur n'existe pas";
+                    echo "</div>";
+                }
+                if ($_GET["pseudo"]==3) {
+                    echo '<div class="alert alert-danger" role="alert">';
+                        echo "Vous ne pouvez pas enregistrer une transaction à vous même!";
+                    echo "</div>";
+                }
             }
-            if ($_GET["pseudo"]==2) {
-                echo '<div class="alert alert-danger" role="alert">';
-                    echo "Cet utilisateur n'existe pas";
-                echo "</div>";
-            }
-            if ($_GET["pseudo"]==3) {
-                echo '<div class="alert alert-danger" role="alert">';
-                    echo "Vous ne pouvez pas enregistrer une transaction à vous même!";
-                echo "</div>";
-            }
-        }
-        ?>
+          ?>
         </div>
       </div>
       <div class="row">
@@ -91,128 +90,126 @@
         <div class="col-sm-8">
           <?php
             if (isset($_GET["montant"])) {
-                if ($_GET["montant"]==1) {
-                    echo '<div class="alert alert-danger" role="alert">';
-                        echo "Vous devez renseigner un montant!";
-                    echo "</div>";
-                }
-                
+              if ($_GET["montant"]==1) {
+                echo '<div class="alert alert-danger" role="alert">';
+                  echo "Vous devez renseigner un montant!";
+                echo "</div>";
+              }
+
             }
           ?>
         </div>
       </div>
-
       </br>
       <div class ="row">
-      <div class="col-sm ">
-        <button type="submit" class="btn btn-primary">Ajouter une transaction</button>
-      </div>
+        <div class="col-sm ">
+          <button type="submit" class="btn btn-primary">Ajouter une transaction</button>
+        </div>
       </div>
     </form>
     </br>
-</div>
-</div>
-    <div class="container marketing">
+  </div>
 
-  <div class="titre"><h2>Voir tes transactions</h2></div>
-  <?php
+  <div class="container marketing">
+    <div class="titre"><h2>Voir tes transactions</h2></div>
+      <?php
       
-      $bdd_transac = con();
-      if (mysqli_connect_errno($bdd_transac)) {
-          echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
-      }
-     
-      $ras = mysqli_query($bdd_transac, "SELECT * FROM  Transactions WHERE (User_src=".$_SESSION['ID'].") OR (User_cible=".$_SESSION['ID'].")");
-      echo '<table class="table table-hover table-white">';
+        $bdd_transac = con();
+        if (mysqli_connect_errno($bdd_transac)) {
+            echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
+        }
       
-      echo '<thead>';
-      echo '<tr>';
-      echo '<td>N°</td>';
-      echo '<td>Montant</td>';
-      echo '<td>Utilisateur source</td>';
-      echo '<td>Utilisateur cible</td>';
-      echo '<td>Message explicatif</td>';
-      echo '<td>Date de création</td>';
-      echo '<td>Statut</td>';
-      echo '<td></td>';
-      echo '<td>Actions</td>';
-      echo '<td></td>';
-      echo '</tr>';
-      echo '</thead>';
+        $ras = mysqli_query($bdd_transac, "SELECT * FROM  Transactions WHERE (User_src=".$_SESSION['ID'].") OR (User_cible=".$_SESSION['ID'].")");
+        echo '<table class="table table-hover table-white">';
 
-      echo '<tbody>';
-      
-      $n=1;
-      while ($donnees = ($row = mysqli_fetch_row($ras))){
+        echo '<thead>';
         echo '<tr>';
-        echo '<th scope="row">'.$n.'</th>';
-        echo '<td>'.$donnees[4].'€';
-        //option modification du montant
-        if (isset($_GET["modar"])) {
-          if ($_GET["modar"]==$donnees[0]) {
-            $tabb = mysqli_query($bdd_transac, "SELECT Msg_exp,Montant FROM Transactions WHERE ID='".$donnees[0]."'");
-            $lignee = mysqli_fetch_row($tabb);
-            echo '<form action="" method="post"><input type="number" name="valeur" value="'.$lignee[1].'"/><input type="submit" value="Enregistrer"/></form></td>';
-            if (isset($_POST['valeur'])){
-              $mo = mysqli_query($bdd_transac, "UPDATE Transactions SET Montant='".$_POST['valeur']."' WHERE ID='".$donnees[0]."'");
-              echo("<meta http-equiv='refresh' content='0'; URL=transactions.php");
+        echo '<td>N°</td>';
+        echo '<td>Montant</td>';
+        echo '<td>Utilisateur source</td>';
+        echo '<td>Utilisateur cible</td>';
+        echo '<td>Message explicatif</td>';
+        echo '<td>Date de création</td>';
+        echo '<td>Statut</td>';
+        echo '<td></td>';
+        echo '<td>Actions</td>';
+        echo '<td></td>';
+        echo '</tr>';
+        echo '</thead>';
+
+        echo '<tbody>';
+
+        $n=1;
+        while ($donnees = ($row = mysqli_fetch_row($ras))){
+          echo '<tr>';
+          echo '<th scope="row">'.$n.'</th>';
+          echo '<td>'.$donnees[4].'€';
+          //option modification du montant
+          if (isset($_GET["modar"])) {
+            if ($_GET["modar"]==$donnees[0]) {
+              $tabb = mysqli_query($bdd_transac, "SELECT Msg_exp,Montant FROM Transactions WHERE ID='".$donnees[0]."'");
+              $lignee = mysqli_fetch_row($tabb);
+              echo '<form action="" method="post"><input type="number" name="valeur" value="'.$lignee[1].'"/><input type="submit" value="Enregistrer"/></form></td>';
+              if (isset($_POST['valeur'])){
+                $mo = mysqli_query($bdd_transac, "UPDATE Transactions SET Montant='".$_POST['valeur']."' WHERE ID='".$donnees[0]."'");
+                echo("<meta http-equiv='refresh' content='0'; URL=transactions.php");
+              }
+            } else {
+              echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymontant&id=".$donnees[0]."'> Modifier</a></td>";
             }
           } else {
             echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymontant&id=".$donnees[0]."'> Modifier</a></td>";
           }
-        } else {
-          echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymontant&id=".$donnees[0]."'> Modifier</a></td>";
-        }
-        echo '</td>';
+          echo '</td>';
 
-        $res1 = mysqli_query($bdd_transac, "SELECT Pseudo FROM Utilisateur WHERE Utilisateur.ID='".$donnees[2]."'");
-        $raw1 = mysqli_fetch_row($res1);
-        echo '<td>'.$raw1[0].'</td>';
+          $res1 = mysqli_query($bdd_transac, "SELECT Pseudo FROM Utilisateur WHERE Utilisateur.ID='".$donnees[2]."'");
+          $raw1 = mysqli_fetch_row($res1);
+          echo '<td>'.$raw1[0].'</td>';
 
-        $res1 = mysqli_query($bdd_transac, "SELECT Pseudo FROM Utilisateur WHERE Utilisateur.ID='".$donnees[3]."'");
-        $raw1 = mysqli_fetch_row($res1);
-        echo '<td>'.$raw1[0].'</td>';
+          $res1 = mysqli_query($bdd_transac, "SELECT Pseudo FROM Utilisateur WHERE Utilisateur.ID='".$donnees[3]."'");
+          $raw1 = mysqli_fetch_row($res1);
+          echo '<td>'.$raw1[0].'</td>';
 
 
-        echo '<td>'.$donnees[1];
+          echo '<td>'.$donnees[1];
 
 
-        //option modification du msg explicatif
-        if (isset($_GET["modmsg"])) {
-          if ($_GET["modmsg"]==$donnees[0]) {
-            $tab = mysqli_query($bdd_transac, "SELECT Msg_exp,Montant FROM Transactions WHERE ID='".$donnees[0]."'");
-            $ligne = mysqli_fetch_row($tab);
-            echo '<form action="" method="post"><input type="text" name="msg_ex" value="'.$ligne[0].'"/><input type="submit" value="Enregistrer"/></form></td>';
-            if(isset($_POST['msg_ex'])){
-              $moo = mysqli_query($bdd_transac, "UPDATE Transactions SET Msg_exp='".$_POST['msg_ex']."' WHERE ID='".$donnees[0]."'");
-              echo("<meta http-equiv='refresh' content='0'; URL=transactions.php>"); 
-            }
+          //option modification du msg explicatif
+          if (isset($_GET["modmsg"])) {
+            if ($_GET["modmsg"]==$donnees[0]) {
+              $tab = mysqli_query($bdd_transac, "SELECT Msg_exp,Montant FROM Transactions WHERE ID='".$donnees[0]."'");
+              $ligne = mysqli_fetch_row($tab);
+              echo '<form action="" method="post"><input type="text" name="msg_ex" value="'.$ligne[0].'"/><input type="submit" value="Enregistrer"/></form></td>';
+              if(isset($_POST['msg_ex'])){
+                $moo = mysqli_query($bdd_transac, "UPDATE Transactions SET Msg_exp='".$_POST['msg_ex']."' WHERE ID='".$donnees[0]."'");
+                echo("<meta http-equiv='refresh' content='0'; URL=transactions.php>"); 
+              }
+            } else {
+              echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymsg&id=". $donnees[0] . "'> Modifier</a></td>";
+          }
           } else {
             echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymsg&id=". $donnees[0] . "'> Modifier</a></td>";
-        }
-        } else {
-          echo "</br><a class='btn btn-secondary btn-sm' role='button' href='action_transaction.php?action=modifymsg&id=". $donnees[0] . "'> Modifier</a></td>";
-        }
-        echo '</td>';
+          }
+          echo '</td>';
 
-        echo '<td>'.$donnees[5].'</td>';
-        if ($donnees[6] == 0){
-          echo '<td>Non réglée<td>';
-        }elseif ($donnees[6] == 1){
-          echo '<td>Réglée<td>';
-        }elseif ($donnees[6] == 2){
-          echo '<td>Annulée<td>';
+          echo '<td>'.$donnees[5].'</td>';
+          if ($donnees[6] == 0){
+            echo '<td>Non réglée<td>';
+          }elseif ($donnees[6] == 1){
+            echo '<td>Réglée<td>';
+          }elseif ($donnees[6] == 2){
+            echo '<td>Annulée<td>';
+          }
+          echo "<td><a class='btn btn-primary' role='button' href='action_transaction.php?action=regler&id=". $donnees[0] . "'> Régler</a></td>";
+          echo "<td><a class='btn btn-primary' role='button' href='action_transaction.php?action=annuler&id=". $donnees[0] . "'> Annuler</a></td>";
+          echo '</tr>';
+          $n=$n+1;
         }
-        echo "<td><a class='btn btn-primary' role='button' href='action_transaction.php?action=regler&id=". $donnees[0] . "'> Régler</a></td>";
-        echo "<td><a class='btn btn-primary' role='button' href='action_transaction.php?action=annuler&id=". $donnees[0] . "'> Annuler</a></td>";
-        echo '</tr>';
-        $n=$n+1;
-      }
+      ?>
+      </tbody>
+      </table>
 
-      
-    ?>
-
-</div>
+  </div>
 
 
 

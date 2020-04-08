b502d8db6ab5e44a5490d889d5c62127d67875d5
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2020-04-08 12:14:21 +0200

    petit fix

--- transactions_ami.php
+++ transactions_ami.php
@@ -11,9 +11,24 @@
     include("ressource/header.php");
   ?>
 
+    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-6 pt-4 px-0">
+            <h1 class="display-5 font-italic"><p id="welcome">Gestion des transactions avec ton ami :
+            <?php 
+            $bdd_transac = con();
+            if (mysqli_connect_errno($bdd_transac)) {
+                echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();
+            }
+            $req = mysqli_query($bdd_transac, "SELECT Pseudo FROM Utilisateur WHERE ID='".$_GET['id']."'");
+            $row = mysqli_fetch_row($req);
+            echo "</br>";
+            echo $row[0];
+            ?>
+            </p></h1>
+        </div>
+    </div>
 
   <div class="container marketing">
-    <div class="titre"><h2>Tes transactions avec </h2></div>
 
       <?php
       

54a7c2ffee8ec7b3199b72952c970a72c3da93ed
gberger-98 <gabriel.berger98@gmail.com>
2020-04-16 14:54:56 +0200

    Augmentation du score bettercodehub

--- fonction_transac.php
+++ fonction_transac.php
@@ -173,7 +173,7 @@ function display_transac($ras,$bdd_transac,$page) {
 }
 
 
-function check_arg_transac($msg_ex,$pseudo,$montant,$bdd_transac) {
+function check_arg_transac($msg_ex,$pseudo,$montant) {
     $bdd_transac = con();
     if (mysqli_connect_errno($bdd_transac)) {
         echo "Echec lors de la connexion à MySQL : " . mysqli_connect_error();

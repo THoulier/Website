9aaebc14d031bd88dc8a7ed1918a96ff9e94c0b3
gberger-98 <gabriel.berger98@gmail.com>
2020-04-24 14:35:32 +0200

    quick fix

--- validation_ajout_transaction.php
+++ validation_ajout_transaction.php
@@ -1,6 +1,6 @@
 
 <?php
-include("ressource/fonction/fonction_transac.php");
+include("ressource/fonction/fonction_error.php");
 session_start();
 $bdd_transac = con();
 if (mysqli_connect_errno($bdd_transac)) {
